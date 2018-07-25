038da93cdf49485c5cc4c88ba7d05b9db52e06d6
Rex Gibson <noamt1@gmail.com>
2018-07-25 17:31:13 -0400

    Update configuration.py

--- lookerpy/configuration.py
+++ lookerpy/configuration.py
@@ -56,7 +56,7 @@ class Configuration(object):
         Constructor
         """
         # Default Base url
-        self.host = "https://localhost:9999/api/3.0"
+        self.host = "https://looker-stage-vpc.knewton.net:29999/api/3.0"
         # Default api client
         self.api_client = None
         # Temp file folder for downloading files
